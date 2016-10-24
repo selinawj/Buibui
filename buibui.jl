9382e8307da191564e89bea660930b0a293b1170
selinawj <selinawj@msn.com>
2016-10-24 23:03:05 +0800

    activities time ranges 20 - 60 min

--- buibui.jl
+++ buibui.jl
@@ -9,8 +9,8 @@ header = csvdata[2]
 activityName = data[:,1]
 METS = data[:,2]
 time = data[:,3]
-targetTime = data[:,4]
-targetMET = data[:,5]
+targetTime = data[1,4]
+targetMET = data[1,5]
 BMI = data[:,6]
 N = length(activityName)
 
@@ -23,13 +23,22 @@ problem = Model(solver = GurobiSolver())
 @objective(problem, Max, sum{METS[i] * x[i], i = 1:N})
 
 #Setting the constraints
-@constraint(problem, sum{METS[i] * x[i], i = 1:N} <= 2000)
-@constraint(problem, sum{time[i] * x[i], i = 1:N} <= 60)
+@constraint(problem, sum{METS[i] * x[i], i = 1:N} <= targetMET) #target MET
+@constraint(problem, sum{time[i] * x[i], i = 1:N} <= targetTime) #target time limit
 
 print(problem)
 solve(problem)
 
-println("Optimal Solutions:")
-for i=1:N
-  println(activityName[i] ,"= ", getvalue(x[i]))
+#Display Optimal Binary Solutions
+# println("Optimal Solutions:")
+# for i=1:N
+#   println(activityName[i] ,"= ", getvalue(x[i]))
+# end
+
+#Print exercises that are selected
+println("Exercises you should do:")
+for i = 1:N
+    if getvalue(x[i]) == 1
+        println("Please do ", activityName[i], " for ", time[i], " minutes.")
+   end
 end
